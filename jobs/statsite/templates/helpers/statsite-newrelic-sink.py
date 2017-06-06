88eafd48eeb7639cbc9bfa418fa70b18b990090a
Bhuvaneswari Ramkumar <bhuvaneswari.ramkumar@ge.com>
2017-06-06 11:01:54 -0700

    newrelic sink configuration changes.

--- jobs/statsite/templates/helpers/statsite-newrelic-sink.py
+++ jobs/statsite/templates/helpers/statsite-newrelic-sink.py
@@ -4,7 +4,6 @@ import requests
 try: import simplejson as json
 except ImportError: import json
 
-
 lines = sys.stdin.read().split("\n")
 metrics = [l.split("|") for l in lines]
 
@@ -13,21 +12,26 @@ metrics = metrics[:-1]
 
 json_dict = {}
 json_text = ""
+num_metrics = 0
 for key, value, timestamp in metrics:
     print (key, value, timestamp)
     key = key.replace(".","_")
-    #print key, value, timestamp
     json_dict["eventType"] = key
     json_dict["amount"] = float(value) 
     json_dict["account"] = timestamp
     json_text += json.JSONEncoder().encode(json_dict) + "," + "\n"
+    num_metrics++
 
 json_text = "[" + json_text + "]"
-url     = 'https://insights-collector.newrelic.com/v1/accounts/NEWRELIC_ACCOUNT_ID/events'
-payload = json_text
-headers = {"Content-Type": "application/json", "X-Insert-Key": "NEWRELIC_API_KEY"}
-try:
-    res = requests.post(url, data=payload, headers=headers)
-except requests.exceptions.RequestException as e: 
-    print (e)
-    sys.exit(1)
+
+if num_metrics > 0:
+    url     = 'https://insights-collector.newrelic.com/v1/accounts/NEWRELIC_ACCOUNT_ID/events'
+    payload = json_text
+    headers = {"Content-Type": "application/json", "X-Insert-Key": "NEWRELIC_API_KEY"}
+    try:
+        res = requests.post(url, data=payload, headers=headers)
+    except requests.exceptions.RequestException as e:
+        print (e)
+        sys.exit(1)
+
+sys.exit(0)
